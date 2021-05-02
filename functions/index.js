825d813db4cd9eb83dc25a8117fde4c9d81ad721
acisotto <61620892+acisotto@users.noreply.github.com>
2021-05-01 21:57:27 -0300

    Alvaro - Insert user and location references

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,10 @@ admin.initializeApp();
 
 const db = admin.firestore()
     .collection("events");
-
+const dbUser = admin.firestore()
+    .collection("users");
+const dbLoc = admin.firestore()
+    .collection("locations");
 
 app.use("/api/v1", router);
 
@@ -30,7 +33,7 @@ router.get("/events/byGroup/:groupID", (request, response) => {
               eventName: evData.eventName == null ?
               "" : evData.eventName,
               locationID: evData.locationID == null ?
-              "": evData.locationID,
+              "" : evData.locationID,
               confirmedUsers: evData.confirmedUsers == null ?
               [""] : evData.confirmedUsers,
               startDate: evData.startDate == null ?
@@ -43,7 +46,7 @@ router.get("/events/byGroup/:groupID", (request, response) => {
               [""] : evData.createDate.toDate(),
               // .toLocaleDateString("pt-BR"),
               userID: evData.userID == null ?
-               "": evData.userID,
+              "" : evData.userID,
             });
           });
           response.json(listEvents);
@@ -70,7 +73,7 @@ router.get("/events/byLocation/:locationID", (request, response) => {
               eventName: evData.eventName == null ?
               "" : evData.eventName,
               locationID: evData.locationID == null ?
-              "": evData.locationID,
+              "" : evData.locationID,
               confirmedUsers: evData.confirmedUsers == null ?
               [""] : evData.confirmedUsers,
               startDate: evData.startDate == null ?
@@ -83,7 +86,7 @@ router.get("/events/byLocation/:locationID", (request, response) => {
               [""] : evData.createDate.toDate(),
               // .toLocaleDateString("pt-BR"),
               userID: evData.userID == null ?
-               "": evData.userID,
+              "" : evData.userID,
             });
           });
           response.json(listEvents);
@@ -106,7 +109,7 @@ router.get("/events/:id", (request, response) => {
         eventName: event.data().eventName == null ?
         "" : event.data().eventName,
         locationID: event.data().locationID == null ?
-        "": event.data().locationID,
+        "" : event.data().locationID,
         confirmedUsers: event.data().confirmedUsers == null ?
         [""] : event.data().confirmedUsers,
         startDate: event.data().startDate == null ?
@@ -119,48 +122,143 @@ router.get("/events/:id", (request, response) => {
         [""] : event.data().createDate.toDate(),
         // .toLocaleDateString("pt-BR"),
         userID: event.data().userID == null ?
-        "": event.data().userID,
+        "" : event.data().userID,
       })
           .catch((error) => response.status(400)
               .send(`Cannot get event: ${error}`)));
 });
 
-router.get("/events", (request, response) => {
-  db.get()
-      .then((events) => {
-        const listEvents = [];
+router.get("/events", async (request, response) => {
+  const events = await db.get();
 
-        events.forEach((ev) => {
-          listEvents.push({
-            id: ev.id,
-            groupID: ev.data().groupID == null ?
-            "" : ev.data().groupID,
-            eventName: ev.data().eventName == null ?
-            "" : ev.data().eventName,
-            locationID: ev.data().locationID == null ?
-            "": ev.data().locationID,
-            confirmedUsers: ev.data().confirmedUsers == null ?
-              [""] : ev.data().confirmedUsers,
-            startDate: ev.data().startDate == null ?
-            [""] : ev.data().startDate.toDate(),
-            endDate: ev.data().endDate == null ?
-             [""] : ev.data().endDate.toDate(),
-            createDate: ev.data().createDate == null ?
-             [""] : ev.data().createDate.toDate(),
-            userID: ev.data().userID == null ?
-             "": ev.data().userID,
-          });
-        });
-
-        response.json(listEvents);
+  if (!events.empty) {
+    const listEvents = [];
+    const tempEvents = [];
+    events.forEach((ev) => {
+      tempEvents.push({
+        id: ev.id,
+        groupID: ev.data().groupID == null ?
+          "" : ev.data().groupID,
+        eventName: ev.data().eventName == null ?
+          "" : ev.data().eventName,
+        locationID: ev.data().locationID == null ?
+          "" : ev.data().locationID,
+        confirmedUsers: ev.data().confirmedUsers == null ?
+          [""] : ev.data().confirmedUsers,
+        startDate: ev.data().startDate == null ?
+          [""] : ev.data().startDate.toDate(),
+        endDate: ev.data().endDate == null ?
+          [""] : ev.data().endDate.toDate(),
+        createDate: ev.data().createDate == null ?
+          [""] : ev.data().createDate.toDate(),
+        userID: ev.data().userID == null ?
+          "" : ev.data().userID,
       });
+    });
+
+    for (const event of tempEvents) {
+      // groups.forEach((group) => {
+      // if (tempGroups[group] != null) {
+      const eventData = event;
+
+      let _location = {
+        locationName: null,
+        address: null,
+        geolocation:
+        {
+          "latitude":
+            null,
+          "longitude":
+            null,
+        },
+        imageUrl: null,
+      };
+      let _userName;
+
+      try {
+        _userName = await dbUser.doc(eventData.userID).get()
+            .then((user) => {
+              return user.data().userName == null ?
+              "" : user.data().userName;
+            });
+      } catch (error) {
+        _userName = "";
+      }
+
+      try {
+        _location = await dbLoc.doc(eventData.locationID).get()
+            .then((location) => {
+              return {
+                locationName: location.data().locationName != null ?
+                location.data().locationName : null,
+                address: location.data().address != null ?
+                location.data().address : null,
+                geolocation:
+              {
+                "latitude": location.data().geolocation.latitude != null ?
+                  location.data().geolocation.latitude.toString() : null,
+                "longitude": location.data().geolocation.longitude != null ?
+                  location.data().geolocation.longitude.toString() : null,
+                "imageUrl": location.data().imageUrl != null ?
+                  location.data().imageUrl : null,
+              },
+
+              };
+            });
+      } catch (error) {
+        _location = {
+          locationName: null,
+          address: null,
+          geolocation:
+          {
+            "latitude":
+              null,
+            "longitude":
+              null,
+          },
+          imageUrl: null,
+        };
+      }
+
+      listEvents.push({
+        id: event.id,
+        groupID: eventData.groupID,
+        eventName: eventData.eventName,
+        locationID: eventData.locationID,
+        confirmedUsers: eventData.confirmedUsers,
+        startDate: eventData.startDate,
+        endDate: eventData.endDate,
+        createDate: eventData.createDate,
+        userID: eventData.userID,
+        userName: _userName,
+        locationName: _location.locationName == null ?
+          "" : _location.locationName,
+        address: _location.address == null ?
+          "" : _location.address,
+        geolocation:
+        {
+          "latitude": _location.geolocation.latitude != null ?
+            _location.geolocation.latitude.toString() : null,
+          "longitude": _location.geolocation.longitude != null ?
+            _location.geolocation.longitude.toString() : null,
+        },
+        imageUrl: _location.imageUrl == null ?
+          "" : _location.imageUrl,
+      });
+      // }
+    }
+    response.json(listEvents);
+  } else {
+    response.send("Events not found");
+  }
 });
 
 
-router.post("/events", (request, response) => {
+router.post("/events", async (request, response) => {
   const startDate = new Date(Date.parse(request.body.startDate));
   const endDate = new Date(Date.parse(request.body.endDate));
   const actualDate = new Date(Date.now());
+  let _userName;
 
   const newEvent = {
     "userID": request.body.userID,
@@ -170,9 +268,24 @@ router.post("/events", (request, response) => {
     "endDate": endDate,
     "createDate": actualDate,
   };
+
+  try {
+    _userName = await dbUser.doc(request.body.userID).get()
+        .then((user) => {
+          return user.data().userName == null ?
+          "" : user.data().userName;
+        });
+  } catch (error) {
+    _userName = null;
+  }
+
   db.add(newEvent)
-      .then(() => {
-        response.status(200).json("Success Added");
+      .then((event) => {
+        newEvent.id = event.id;
+        newEvent.userName = _userName;
+        response.status(200).json(newEvent);
+      }).catch((e) => {
+        response.status(500);
       });
 });
 
@@ -199,7 +312,7 @@ router.patch("/events/:id", (request, response) => {
     if (body.locationID) newEvent.locationID = body.locationID;
     if (body.startDate) {
       const startDate = new Date(Date.parse(body.startDate));
-      newEvent.startDate =startDate;
+      newEvent.startDate = startDate;
     }
     if (body.endDate) {
       const endDate = new Date(Date.parse(body.endDate));
@@ -207,7 +320,7 @@ router.patch("/events/:id", (request, response) => {
     }
     db.doc(request.params.id).update(newEvent)
         .then(
-            (event)=>response.send(`${event.id} updated sucessfully`)
+            (event) => response.send(`${event.id} updated sucessfully`),
         );
     //     .then((event)=>
     //       response.send(event.data()));
